357a6e83a9f56d027ef9000c8b50f771f018f94c
tiagofpr <tiagofpr@outlook.com>
2026-09-19 21:22:19 -0300

    tipo de dados basicos em python - Santander

---
+++ tipos_de_dados.py
@@ -0,0 +1,19 @@
+# 2. Noções básicas de Python
+
+# Tipos de dados básicos em Python
+# Inteiros (int)
+
+idade =25
+quantidade = 100
+
+#Flutuantes (float)
+preco = 9.99
+altura = 1.75
+
+#Cadeia textos (Strings)
+nome = "Tiago"
+mensagem = 'Ola turma'
+
+#Booleanos (bool)
+maior_de_idade = True
+tem_desconto= False
